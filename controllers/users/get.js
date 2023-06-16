4f0f1b14a6aa344c28a6195af809243f7444ee8c
RomanCherkunov <roman4erkunov@mail.ru>
2023-06-16 11:21:28 +0300

    Переделал гет запрос по квэри(добавил апперкейсы и инклюдс)

--- controllers/users/get.js
+++ controllers/users/get.js
@@ -2,9 +2,11 @@ const usersData = require("../../usersData");
 
 const get = (req, res) => {
   if (req.query.location) {
-    const newData = usersData.filter(
-      (item) => item.location === req.query.location
-    );
+    const location = req.query.location.toUpperCase();
+    const newData = usersData.filter((item) => {
+      const itemLocation = item.location.toUpperCase();
+      return itemLocation.includes(location);
+    });
     res.send(newData);
   } else {
     res.send(usersData);
